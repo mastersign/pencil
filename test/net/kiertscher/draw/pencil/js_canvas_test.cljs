c51861f8a100547a2838660a43368ea90412de77
Tobias Kiertscher <dev@mastersign.de>
2016-09-07 21:59:08 +0200

    implemented difference computation in browser

---
+++ test/net/kiertscher/draw/pencil/js_canvas_test.cljs
@@ -0,0 +1,48 @@
+(ns net.kiertscher.draw.pencil.js-canvas-test
+  (:require [net.kiertscher.draw.pencil-test :refer [test-drawings]]))
+
+(defn- get-el
+  [id]
+  (.getElementById js/document id))
+
+(defn- pixel-difference
+  [a1 a2 ar i]
+  (let [ir i
+        ig (+ i 1)
+        ib (+ i 2)
+        ia (+ i 3)
+        color-diff (not (and (= (aget a1 ir) (aget a2 ir))
+                             (= (aget a1 ig) (aget a2 ig))
+                             (= (aget a1 ib) (aget a2 ib))))
+        alpha-diff (not (= (aget a1 ia) (aget a2 ia)))]
+    (aset ar ir (if color-diff 220 (if alpha-diff 255 128)))
+    (aset ar ig (if color-diff 0 (if alpha-diff 128 255)))
+    (aset ar ib (if color-diff 0 (if alpha-diff 0 128)))
+    (aset ar ia 255)))
+
+(defn- image-data-difference
+  [img-data-1 img-data-2]
+  (let [w (.-width img-data-1)
+        h (.-height img-data-1)
+        a1 (.-data img-data-1)
+        a2 (.-data img-data-2)]
+    (doseq [y (range h)
+            x (range w)]
+      (pixel-difference a1 a2 a2
+                        (* 4 (+ x (* y w)))))))
+
+(do
+  (doseq [[id {:keys [w h]}] test-drawings]
+    (let [n (name id)
+          can (get-el n)
+          img-awt (get-el (str n "-awt"))
+          can-diff (get-el (str n "-diff"))
+          ctx (.getContext can "2d")
+          ctx-diff (.getContext can-diff "2d")]
+      (set! (.-width can-diff) w)
+      (set! (.-height can-diff) h)
+      (.drawImage ctx-diff img-awt 0 0)
+      (let [data (.getImageData ctx 0 0 w h)
+            data-diff (.getImageData ctx-diff 0 0 w h)]
+        (image-data-difference data data-diff)
+        (.putImageData ctx-diff data-diff 0 0)))))
