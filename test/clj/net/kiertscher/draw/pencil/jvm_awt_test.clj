5db7faf202479c14d0e2af521f4178c0bbd4e255
Tobias Kiertscher <dev@mastersign.de>
2016-10-17 17:03:08 +0200

    code formatting

--- test/clj/net/kiertscher/draw/pencil/jvm_awt_test.clj
+++ test/clj/net/kiertscher/draw/pencil/jvm_awt_test.clj
@@ -1,7 +1,6 @@
 (ns net.kiertscher.draw.pencil.jvm-awt-test
-  (:require
-    [net.kiertscher.draw.pencil-test :as t]
-    [net.kiertscher.draw.pencil.jvm-awt :as awt])
+  (:require [net.kiertscher.draw.pencil-test :as t]
+            [net.kiertscher.draw.pencil.jvm-awt :as awt])
   (:import [java.io File]))
 
 (defn draw
@@ -9,7 +8,7 @@
   (.mkdirs (File. "out/test/awt"))
   (awt/render-in-file
     (str "out/test/awt/" id ".png") "PNG"
-    w h f)  )
+    w h f))
 
 (doseq [[id {:keys [w h f]}] t/test-sketches]
   (draw (name id) w h f))
